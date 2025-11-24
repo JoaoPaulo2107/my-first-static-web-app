8ff0908a74373401faaa9001fdcb69632efcc277
joaopaulo2107 <joao.p.azevedo2107@gmail.com>
2025-11-24 14:15:04 -0300

    Commit made from VS Code Azure Static Web Apps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'joão';
-  return <div>olá {value}</div>;
+  const value = 'João e Zaia';
+  return <div>Olá {value}</div>;
 }
 
 export default App;
